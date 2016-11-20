d10e9efe67018a2c464fbf13c40aa8bf1f1dab2e
Aleksey Romanov <lex.at.net@gmail.com>
2016-11-20 21:56:57 +0600

    fix ctags insallation

--- init.sh
+++ init.sh
@@ -8,3 +8,17 @@ apt-get update && apt-get install -y \
         emacs-snapshot \
         mc
 
+apt-get update
+apt-get install -y git 
+apt-get install -y make
+apt-get install -y autoconf
+apt-get install -y pkg-config
+
+#install ctags from sources
+cd /tmp
+git clone https://github.com/universal-ctags/ctags.git
+cd ctags
+./autogen.sh 
+./configure
+make
+make install
