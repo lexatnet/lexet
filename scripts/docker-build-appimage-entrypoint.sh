8ff23ab08b1ad8c74fc9d413b1ac3c1bfb41490a
lexatnet <lex.at.net@gmail.com>
2019-10-09 00:10:01 +0600

    fix docker builder for appimage

--- scripts/docker-build-appimage-entrypoint.sh
+++ scripts/docker-build-appimage-entrypoint.sh
@@ -8,6 +8,14 @@ apt-get update
 apt-get install -y wget
 apt-get install -y desktop-file-utils
 apt-get install -y file
+export DEBIAN_FRONTEND=noninteractive
+#TZ= Asia/Omsk
+#install tzdata package
+apt-get install -y tzdata
+# set your timezone
+ln -fs /usr/share/zoneinfo/Asia/Omsk /etc/localtime
+dpkg-reconfigure --frontend noninteractive tzdata
+apt-get install -y glib2.0
 rm -rf /var/lib/apt/lists/*
 
 cd $through_point
